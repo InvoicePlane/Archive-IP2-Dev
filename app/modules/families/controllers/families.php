c534ff3bb1961729cbecabf9a766f4112882989d
Kovah <mail@kovah.de>
2016-01-08 18:08:38 +0100

    Added PHP docs to the product families module

--- app/modules/families/controllers/families.php
+++ app/modules/families/controllers/families.php
@@ -1,12 +1,17 @@
 <?php
-
 if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-
+/**
+ * Class Families
+ * @package Modules\Families\Controllers
+ */
 class Families extends Admin_Controller
 {
+    /**
+     * Families constructor.
+     */
     public function __construct()
     {
         parent::__construct();
@@ -14,6 +19,10 @@ class Families extends Admin_Controller
         $this->load->model('mdl_families');
     }
 
+    /**
+     * Index page, returns all product families
+     * @param int $page
+     */
     public function index($page = 0)
     {
         $this->mdl_families->paginate(site_url('families/index'), $page);
@@ -24,6 +33,12 @@ class Families extends Admin_Controller
         $this->layout->render();
     }
 
+    /**
+     * Returns the form
+     * If an ID was provided the form will be filled with the data of the product family
+     * for the given ID and can be used as an edit form.
+     * @param null $id
+     */
     public function form($id = null)
     {
         if ($this->input->post('btn_cancel')) {
@@ -55,10 +70,13 @@ class Families extends Admin_Controller
         $this->layout->render();
     }
 
+    /**
+     * Deletes a client from the database based on the given ID
+     * @param $id
+     */
     public function delete($id)
     {
         $this->mdl_families->delete($id);
         redirect('families');
     }
-
 }
--- app/modules/families/models/mdl_families.php
+++ app/modules/families/models/mdl_families.php
@@ -1,25 +1,37 @@
 <?php
-
 if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-
+/**
+ * Class Mdl_Families
+ * @package Modules\Families\Models
+ */
 class Mdl_Families extends Response_Model
 {
     public $table = 'ip_families';
     public $primary_key = 'ip_families.family_id';
 
+    /**
+     * The default select directive used in every query
+     */
     public function default_select()
     {
         $this->db->select('SQL_CALC_FOUND_ROWS *', false);
     }
 
+    /**
+     * The default order by directive used in every query
+     */
     public function default_order_by()
     {
         $this->db->order_by('ip_families.family_name');
     }
 
+    /**
+     * Returns the validation rules for product families
+     * @return array
+     */
     public function validation_rules()
     {
         return array(
@@ -30,5 +42,4 @@ class Mdl_Families extends Response_Model
             )
         );
     }
-
 }
