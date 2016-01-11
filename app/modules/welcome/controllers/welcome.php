b885c9dd509967a839db6d16cb0ab2c91b4493d8
Kovah <mail@kovah.de>
2016-01-11 14:36:48 +0100

    Added PHP docs to the welcome module

--- app/modules/welcome/controllers/welcome.php
+++ app/modules/welcome/controllers/welcome.php
@@ -3,21 +3,15 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-/*
- * InvoicePlane
- *
- * A free and open source web based invoicing system
- *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2015 InvoicePlane.com
- * @license		https://invoiceplane.com/license.txt
- * @link		https://invoiceplane.com
- *
+/**
+ * Class Welcome
+ * @package Modules\Welcome\Controllers
  */
-
 class Welcome extends CI_Controller
 {
+    /**
+     * Returns the welcome page
+     */
     public function index()
     {
         $this->load->view('welcome');
