19d47d209f27c6ca520cc6c4a1921cdb9ed5f641
Kovah <mail@kovah.de>
2016-01-25 16:01:27 +0100

    Updated the base layout to work with the setup

--- app/modules/layout/views/base.php
+++ app/modules/layout/views/base.php
@@ -2,7 +2,19 @@
 <html class="no-js" lang="en">
 
 <head>
-    <?php $this->view('includes/header') ?>
+    <title>InvoicePlane</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="robots" content="NOINDEX,NOFOLLOW">
+
+    <link rel="icon" type="image/png" href="<?php echo THEME_URL; ?>img/favicon.png">
+
+    <link rel="stylesheet" href="<?php echo THEME_URL; ?>css/app.min.css">
+
+    <script src="<?php echo base_url(); ?>themes/core/js/dependencies.min.js"></script>
+    <script src="<?php echo base_url(); ?>themes/core/js/app.min.js"></script>
 </head>
 
 <body>
